db308a857a847f2f3e764b9edd6ef1ebb49c926f
chenkins <chenkins44@gmail.com>
2026-05-22 14:51:52 +0200

    fix: compatibility with pydantic v2.

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from fractions import Fraction
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic.json import ENCODERS_BY_TYPE
+from pydantic import BaseModel
 
 from app.routes import router
 
@@ -17,12 +17,8 @@ middleware_config = {
     "allow_headers": ["*"],
 }
 
-
-def fraction_encoder(obj: Fraction) -> typing.Any:
-    return {'__fraction__': True, 'as_str': str((obj.numerator, obj.denominator))}
-
-
-ENCODERS_BY_TYPE[Fraction] = fraction_encoder
+# https://github.com/pydantic/pydantic/issues/8426
+BaseModel.model_config["json_encoders"] = {Fraction: lambda obj: {'__fraction__': True, 'as_str': str((obj.numerator, obj.denominator))}}
 
 app = FastAPI()
 app.add_middleware(
