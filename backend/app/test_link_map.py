ade62f0859637418f90209be905c8938f52ab75b
chenkins <chenkins44@gmail.com>
2026-06-22 17:33:13 +0200

    refactor(backend): fix import.

--- backend/app/test_link_map.py
+++ backend/app/test_link_map.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 
 from app.routes import build_stations_and_links_payload, extract_link_map
-from env_generation.env_generator import env_generator
+from flatland.env_generation.env_generator import env_generator
 
 material = [(0, [(2, 8), (1, 8), (1, 9), (1, 10), (1, 11), (1, 12), (1, 13), (1, 14), (1, 15), (2, 15), (3, 15), (4, 15), (5, 15), (6, 15), (7, 15), (8, 15),
                  (9, 15), (10, 15), (11, 15), (12, 15), (13, 15), (14, 15), (15, 15), (15, 14), (15, 13), (15, 12), (15, 11), (15, 10), (15, 9), (15, 8),
